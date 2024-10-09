d6f6ce06178a67aebda3bc67e5250cffcee4fe46
Duc3rd <165653481+Duc3rd@users.noreply.github.com>
2024-10-09 11:56:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import TodoList from './TodoList/TodoList';
 import ImageSearch from './ImageSearch/ImageSearch';
 import RandomColor from './RandomColor/RandomColor';
+import './App.css'; 
 
-const App = () => {
-  return (
-    <div>
-      <TodoList />
-      <ImageSearch />
-      <RandomColor />
-    </div>
-  );
-};
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="todolist-container">
+                    <TodoList />
+                </div>
+                <div className="image-search-container">
+                    <h2>Tìm Kiếm Hình Ảnh</h2>
+                    <ImageSearch />
+                </div>
+                <div className="random-color-container">
+                    <h2>Random Color</h2>
+                    <RandomColor />
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default App;
